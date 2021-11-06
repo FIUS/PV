e8080884f86ffd10cd559cea6e77b47c2978718f
schieljn <diemilbe@gmx.net>
2021-11-06 22:36:59 +0100

    fixed last fg-inf to fius

--- backend/util.py
+++ backend/util.py
@@ -10,5 +10,5 @@ def build_response(message: object, code: int = 200, type: str = "application/js
     """
     r=Response(response=json.dumps(message), status=code, mimetype=type)
     if cookie:
-        r.set_cookie(cookie[0],cookie[1],domain="pv.fg-inf.de",max_age=cookie_expire)
+        r.set_cookie(cookie[0],cookie[1],domain="pv.fius.de",max_age=cookie_expire)
     return r
