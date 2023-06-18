5e4bed9358834debffac61b059111c5bd20c5039
jschiel <justin.schiel@iteratec.com>
2023-06-19 00:40:35 +0200

    merge

--- backend/TaskScheduler.py
+++ backend/TaskScheduler.py
@@ -4,16 +4,17 @@ import threading
 
 
 class TaskScheduler:
-    def __init__(self) -> None:
-        return
+    def __init__(self, app) -> None:
+        self.app = app
 
     def start(self) -> None:
         threading.Thread(target=self.loop).start()
 
     def loop(self) -> None:
-        while True:
-            time.sleep(60*5)
-            schedule.run_pending()
+        with self.app.app_context():
+            while True:
+                time.sleep(60*5)
+                schedule.run_pending()
 
     def add_Daily_Task(self, task, *args) -> None:
         if len(args) > 0:
